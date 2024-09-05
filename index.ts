a1d6086373ca788b7e9fa06667749983a980f760
Maira <mairatariq896@gmail.com>
2024-09-05 10:33:09 -0700

    add files

---
+++ index.ts
@@ -0,0 +1,77 @@
+var userName = "maira";
+var number4  = 12
+console.log(number4);
+//camelCase
+var userName = "maira"
+//PascalCase
+var UserName = "samra"
+//lowerCase
+var username = "maira"
+//underscore_case
+var user_name = "samra"
+//stringType (letters)
+//dataType,number  (number)
+//Boolean type (true,false)
+//string types
+//single quotation
+var named:string =('maira');
+//double quotation 
+var username:string = ("maira");
+//backti... quotation
+var named:string =(`maira `);
+//float anteger (having decimal point in it )
+var num4:number = 22.4;
+var num3:number =22;
+console.log
+//if we having two boxes in RAM  with same username 
+//then we save the same data like string 
+var userName:string = "samra";
+userName = "awais";
+console.log(userName);
+//syntaxerror (computererror)
+//logicalerrror(humanthink)
+var named = "maryam";
+console.log(named);
+//Arthimatic operators
+var num1:number = 10
+var num2:number = 5
+var num3:number = num1 * 2 + num2 / num1 + 10 - 20 % 3
+console.log(num3)
+
+//division ,multiplication
+//addition ,subtraction
+//remainder %
+//** (power), means 2 ki power 5 like 2**5
+// full divide ho jeye tu last wla remainder ho ga 
+// agr divide na ho or ist number 
+//7/12 ramainder 7
+// 12/7 
+//++ post increment
+var num1:number = 55
+var result:number = num1++
+num1 = num1 +1;
+console.log("result",result);
+// or
+num1++
+console.log(num1);
+//pre increment 
+++num1
+console.log(num1)
+var num1 =10;
+var result = num1++
+var result2 = ++num1
+console.log(result);
+console.log(result2);
+//pre decrement 
+var a = 20;
+var result1 = --a
+var result2 = a--
+console.log(a);
+console.log(result1);
+console.log(result2);
+
+
+
+
+
+
